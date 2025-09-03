14fb18f3455961e57d839e26f7fbc9f16f9f461f
Hamid Zoraghein <hrz1365@gmail.com>
2025-09-03 15:55:17 -0700

    refactor: change agg column for places

--- src/aggregator.py
+++ src/aggregator.py
@@ -19,21 +19,16 @@ def generate_results(
     agg_cols = [str(i) for i in range(24)]
     agg_exprs = ", ".join([f'sum(weight * "{col}") AS "{col}"' for col in agg_cols])
 
-    # Conditionally include place_state if aggregating by place_name
-    state_col = "place_state," if aggregation_col == "place_name" else ""
-    state_group = ", place_state" if state_col else ""
-
     query = f"""
         SELECT
             {aggregation_col},
-            {state_col}
             charge_category,
             scenario,
             year,
             {agg_exprs},
             CEILING(SUM(ports * weight)) AS ports
         FROM {input_table}
-        GROUP BY {aggregation_col}{state_group}, charge_category, scenario, year
-        ORDER BY {aggregation_col}{state_group}, charge_category, scenario, year
+        GROUP BY {aggregation_col}, charge_category, scenario, year
+        ORDER BY {aggregation_col}, charge_category, scenario, year
     """
     return con.sql(query)
